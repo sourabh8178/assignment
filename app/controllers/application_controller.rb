d68538ad45bd65e47be6f2cd57b7d50195a539c2
sourabh kumar <sourabhkumar20212021@gmail.com>
2023-07-19 14:53:51 +0530

    change in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
  helper_method :current_user
  
  def current_user
-  @current_user ||= User.where(authentication_token: request.headers['Token']).first
+  @current_user ||= User.find_by(authentication_token: request.headers['Token'])
  end
 
  def authenticate_user!
   return render json:{error:'401 Unauthorized!'},status: 401 unless current_user
  end
- 
+
 end
